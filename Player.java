2e38d546f6a2893fc667dad14db9e68a6f211a5e
a-shaker1 <>
2024-10-30 15:59:56 +0100

    Player is now completed

--- Player.java
+++ Player.java
@@ -1,19 +1,49 @@
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Player{
     private String name;
-    private List<Card> hand = new ArrayList<>();
+    private List<Card> hand;
 
     public Player(String name){
         this.name = name;
+        this.hand = new ArrayList<>();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public List<Card> getHand(){
+        return hand;
     }
 
     public void drawCard(Deck deck){
-
+        Card drawnCard = deck.draw();
+        if (drawnCard != null){
+            hand.add(drawnCard);
+        }
     }
 
-    public void playCard(int index){
-        
+    public Card playCard(int Index){
+        if (Index >= 0 && Index <= hand.size()){
+            return hand.remove(Index);
+        }else {
+            System.out.println("invalid Index");
+            return null;
+        }
+    }
+
+    public String showHand(){
+        StringBuilder handDetails = new StringBuilder();
+        for(int i =0; i < hand.size(); i++){
+            Card card = hand.get(i);
+
+            handDetails.append(i);
+
+            handDetails.append(card.toString());
+        }
+        return handDetails.toString();
     }
 }
